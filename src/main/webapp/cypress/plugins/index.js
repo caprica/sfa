9258685e75379af18f3993a781e0e79dc2823ede
Mark Lee <mark.lee@capricasoftware.co.uk>
2020-03-30 08:35:51 +0100

    Show how to customise window placement for Cypress
    By default it maximizes the Chrome window on launch, this can be
    somewhat annoying on an ultra-wide display.

--- src/main/webapp/cypress/plugins/index.js
+++ src/main/webapp/cypress/plugins/index.js
@@ -12,10 +12,24 @@
 // This function is called when a project is opened or re-opened (e.g. due to
 // the project's config changing)
 
+// Starting maximised is canny annoying on an ultra-wide display
+const customWindowPlacement = false
+
 /**
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
+    // `on` is used to hook into various events Cypress emits
+    // `config` is the resolved Cypress config
+
+    if (customWindowPlacement) {
+        on('before:browser:launch', (browser = {}, launchOptions) => {
+            if (browser.family === 'chromium' && browser.name !== 'electron') {
+                launchOptions.args = launchOptions.args.filter(opt => opt !== '--start-maximized')
+                launchOptions.args.push('--window-position=1840,0')
+                launchOptions.args.push('--window-size=1600,1480')
+                return launchOptions
+             }
+        })
+    }
 }
